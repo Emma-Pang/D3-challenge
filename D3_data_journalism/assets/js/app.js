393ee33fbe6de791e3300b89dffb4a6787c85790
Emma Pang <emma.kpang@gmail.com>
2020-03-26 23:29:46 -0400

    added text to bubbles

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,13 +1,13 @@
 // @TODO: YOUR CODE HERE!
 
-var svgWidth = window.innerWidth;
-var svgHeight = window.innerHeight;
+var svgWidth = 900;
+var svgHeight = 500;
 
 var margin = {
   top: 20,
   right: 40,
   bottom: 60,
-  left: 100
+  left: 40
 };
 
 var width = svgWidth - margin.left - margin.right;
@@ -44,7 +44,7 @@ d3.csv("assets/data/data.csv").then(function(healthData) {
       .domain([0, d3.max(healthData, d => d.poverty)])
       .range([0, width]);
     var yLinearScale = d3.scaleLinear()
-      .domain([0, d3.max(healthData, d => d.healthcare)])
+      .domain([0, d3.max(healthData, d => d.healthcare)+2])
       .range([height, 0]);
 
     // Step 3: Create axis functions
@@ -61,66 +61,48 @@ d3.csv("assets/data/data.csv").then(function(healthData) {
     chartGroup.append("g")
       .call(leftAxis);
 
-    // // Step 5: Create Circles
-    // // ==============================
-    // var circlesGroup = chartGroup.selectAll("circle")
-    // .data(healthData)
-    // .enter()
-    // .append("circle")
-    // .attr("cx", d => xLinearScale(d.poverty))
-    // .attr("cy", d => yLinearScale(d.healthcare))
-    // .attr("class", "stateCircle")
-    // .attr("class", "stateText");
-
-    // // Step 6: Initialize tool tip
-    // // ==============================
-    // var toolTip = d3.tip()
-    //   .attr("class", "tooltip")
-    //   .offset([80, -60])
-    //   .html(function(d) {
-    //     var state=d.state;
-    //     var povertyrate = +d.poverty;
-    //     var healthcare= +d.healthcare;
-
-    //     return (`${state}<br>poverty level: ${poverty}<br>healthcare: ${healthcare}`);
-    //   });
-
-    // Step 7: Create tooltip in the chart
-    // ==============================
-    // chartGroup.call(toolTip);
-
-
-  var circles = svg.selectAll('circle')
+      var circles = svg.selectAll('circle')
       .data(healthData)
       .enter()
     .append('circle')
       .attr('cx',function (d) { return xLinearScale(d.poverty) })
       .attr('cy',function (d) { return yLinearScale(d.healthcare) })
       .attr('r','10')
-      .attr('stroke','black')
+      .classed("stateCircle", true)
       .attr('stroke-width',1)
+      .on('mouseover', function () {
+        d3.select(this)
+          .transition()
+          .duration(500)
+          .attr('r',20)
+          .attr('stroke-width',3)
+          
+      })
+      .on('mouseout', function () {
+        d3.select(this)
+          .transition()
+          .duration(500)
+          .attr('r',10)
+          .attr('stroke-width',1)
+      })
 
-    // // circlesGroup.on("mouseover", function(data) {
-    // //   toolTip.show(data);
-    // // })
-    //   // onmouseout event
-    //   .on("mouseout", function(data, index) {
-    //     toolTip.hide(data);
-    //   });
-
-
-
-
-    //   chart.append('g')
-    //   .call(leftAxis);
-
+      var stateText = svg.selectAll("text")
+      .data(healthData)
+      .enter()
+      .append("text")
+      .attr("x", d => xLinearScale(d.poverty))
+      .attr("y", d => yLinearScale(d.healthcare))
+      .text(d=> d.abbr)
+      .classed("stateText",true)
+      .style("font-size", "9px")
+      .attr("dy", ".35em");
 
 
 
     // Create axes labels
     chartGroup.append("text")
       .attr("transform", "rotate(-90)")
-      .attr("y", 0 - margin.left + 40)
+      .attr("y", 0 - margin.left )
       .attr("x", 0 - (height / 2))
       .attr("dy", "1em")
       .attr("class", "aText")
@@ -132,10 +114,6 @@ d3.csv("assets/data/data.csv").then(function(healthData) {
       .text("In Poverty (%)");
 
 
-    chartGroup.append("text")
-		  .style("text-anchor", "center")
-		  .attr("class", "axisText")
-      .text("Healthcare and Poverty in the US");  
     
   }).catch(function(error) {
     console.log(error);
